63625807dd0fad2496ca817daac76bfedab0df74
Pradipta Nag <pradipta.nag2@gmail.com>
2020-08-15 15:41:31 +0530

    Delete functionality

--- JS/final.js
+++ JS/final.js
@@ -1,7 +1,11 @@
 var addForm = document.getElementById('addForm');
 console.log(addForm);
 
+var elements = document.getElementById('items');
+
 addForm.addEventListener('submit', submitForm);
+elements.addEventListener('click', removeItem);
+
 function submitForm(e) {
     e.preventDefault();
     var textBoxContent = document.getElementById('item').value;
@@ -21,4 +25,13 @@ function submitForm(e) {
 
     var listGroup = document.getElementById('items');
     listGroup.appendChild(newListElement);
+    
+}
+
+function removeItem(e) {
+    if(e.target.classList.contains('delete')){
+        console.log('Delete button clicked');
+        var itemToBeRemoved = e.target.parentElement;
+        elements.removeChild(itemToBeRemoved);
+    }
 }
