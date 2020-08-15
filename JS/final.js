8ebac0143cc8aec0540569c302b7c5df9682dc9c
Pradipta Nag <pradipta.nag2@gmail.com>
2020-08-15 15:47:20 +0530

    Filter functionality

--- JS/final.js
+++ JS/final.js
@@ -3,8 +3,11 @@ console.log(addForm);
 
 var elements = document.getElementById('items');
 
+var filter = document.getElementById('filter');
+
 addForm.addEventListener('submit', submitForm);
 elements.addEventListener('click', removeItem);
+filter.addEventListener('keyup', filterItems);
 
 function submitForm(e) {
     e.preventDefault();
@@ -34,4 +37,22 @@ function removeItem(e) {
         var itemToBeRemoved = e.target.parentElement;
         elements.removeChild(itemToBeRemoved);
     }
+}
+
+
+// Filter Items
+function filterItems(e){
+    // convert text to lowercase
+    var text = e.target.value.toLowerCase();
+    // Get lis
+    var items = elements.getElementsByTagName('li');
+    // Convert to an array
+    Array.from(items).forEach(function(item){
+      var itemName = item.firstChild.textContent;
+      if(itemName.toLowerCase().indexOf(text) != -1){
+        item.style.display = 'block';
+      } else {
+        item.style.display = 'none';
+      }
+    });
 }
